0ff457fd49b4e9fe935091bd72294932d17192ae
Douglas da Silva Mota <doug.mtt@gmail.com>
2024-05-21 21:04:21 -0300

    Update 21/05/2024

---
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -0,0 +1,8 @@
+public class MinhaClasse {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Uma classe bem estruturada, não quer guerra com ninguém!");
+    }
+
+}
